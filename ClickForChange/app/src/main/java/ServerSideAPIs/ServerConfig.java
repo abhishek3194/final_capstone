069e1e53b9e7a4408f210b617883a469d7f4c566
pb10 <poornima10b@gmail.com>
2016-10-22 22:37:21 +0530

    Remote Access

--- ClickForChange/app/src/main/java/ServerSideAPIs/ServerConfig.java
+++ ClickForChange/app/src/main/java/ServerSideAPIs/ServerConfig.java
@@ -5,10 +5,6 @@ package ServerSideAPIs;
  */
 public class ServerConfig {
 
-<<<<<<< HEAD
     public static String SERVER = "http://pecfest.in/ClickForChange/";
 
-=======
-    public static String SERVER = "http://192.168.1.5/Click4Change/";
->>>>>>> 7a1be4f83757834164b60a135c3bbc1784fe2e26
 }
